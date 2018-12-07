39128e37229a14e3ed33727cb857061f9ef3528e
duicul <duicul@yahoo.com>
2018-12-07 23:13:20 +0000

    login and .js script

--- flask/Login.py
+++ flask/Login.py
@@ -1,22 +1,33 @@
-from flask import Flask, redirect, url_for, request,render_template
+from flask import Flask,session, redirect, url_for, request,render_template
 app = Flask(__name__)
+app.secret_key = '571ba9$#/~90'
 
 @app.route('/success/<name>')
 def success(name):
    return 'welcome %s' % name
 
+
+
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
+      session['username'] = request.form['nm'] 
+      return redirect('/')
    else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+      sessiom['username'] = request.args.get('nm')
+      return redirect('/')
 
 @app.route('/')
 def index():
-   return render_template('login.html')
+	if 'username' in session:
+		username = session['username']
+		return "<font color=red size=20> Hello "+username+"</font>"+"<br><a href=\"/logout\"><button>Logout</button>"
+	else:  return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('username',None)
+	return redirect('/')
 
 if __name__ == '__main__':
    app.run(debug = True,host='0.0.0.0')
---
+++ flask/static/hello.js
@@ -0,0 +1,7 @@
+function hello(){
+ alert("hello");
+ document.getElementById("messfunc").innerHTML = "hello";}
+
+function goodbye(){
+ alert("goodbye");
+ document.getElementById("messfunc").innerHTML = "goodbye";} 
