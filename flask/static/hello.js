ba99b2065809c92342863228fb14214f195be750
duicul <duicul@yahoo.com>
2018-12-08 10:09:06 +0200

    bootstrap jquerry

--- flask/static/hello.js
+++
@@ -1,7 +0,0 @@
-function hello(){
- alert("hello");
- document.getElementById("messfunc").innerHTML = "hello";}
-
-function goodbye(){
- alert("goodbye");
- document.getElementById("messfunc").innerHTML = "goodbye";} 
---
+++ flask/static/js/hello.js
@@ -0,0 +1,11 @@
+function hello(){
+alert("hello");
+$("#messfunc").html("hello");
+ //document.getElementById("messfunc").innerHTML = "hello";
+ }
+
+function goodbye(){
+ alert("goodbye");
+ $("#messfunc").html("goodbye");
+ //document.getElementById("messfunc").innerHTML = "goodbye";
+ } 
