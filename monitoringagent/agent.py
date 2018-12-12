0972998aa3024aaa35bf711ec764f0eee79bf361
duicul <duicul@yahoo.com>
2018-12-12 23:19:51 +0200

    json init

--- monitoringagent/agent.py
+++ monitoringagent/agent.py
@@ -1,1 +1,15 @@
-
+import requests
+import json
+data="outputpins"
+r = requests.get('http://localhost:8765/outputpins')
+status_json = r.json
+y=json.loads(r.text)
+print(y)
+for i in range(30):
+    try:
+        print(str(i)+" "+y[str(i)])
+    except KeyError:
+        pass
+print(r.text)
+#for i in status_json:
+#    print(i)
